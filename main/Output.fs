d9bf41be1aa4f382c42c6a707b214e349178f7c0
roge111 <64649951+roge111@users.noreply.github.com>
2025-03-01 12:48:11 +0300

    Полное переписание кода

--- main/Output.fs
+++
@@ -1,7 +0,0 @@
-module OutputHandler
-
-let printInterpolationResults (results: (float * float) list) title =
-    printfn "%s:" title
-    
-    results |> List.iter (fun (x, y) -> printfn "(%.3f, %.3f)" x y)
-    List.tryLast results
